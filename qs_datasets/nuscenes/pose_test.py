8dfb593cdf56bd48573c7ac4116bb2b23ad5f291
bruce <rosboy@126.com>
2025-08-19 21:37:36 +0800

    support pytest for qs_datasets/nuscenets/pose

--- qs_datasets/nuscenes/pose_test.py
+++ qs_datasets/nuscenes/pose_test.py
@@ -1,19 +1,33 @@
+import sys
+import pytest
+
 import numpy as np
 
 from qs_datasets.nuscenes import pose
 
 
-def _test():
+def test_pose_transform():
     # Rotation anti-clockwise 90-degree.
     p = pose.Pose(
-        np.array([1/np.sqrt(2), 0, 0, 1/np.sqrt(2)]), np.array([1, 2, 3]),)
+        np.array([1 / np.sqrt(2), 0, 0, 1 / np.sqrt(2)]),
+        np.array([1, 2, 3]),
+    )
     # [1 0 0] -> [0, 1, 0], add [1, 2, 3] -> [1, 3, 3]
     t_p = p.transform(np.array([1, 0, 0]))
-    assert np.all(t_p == [1, 3, 3])
-    print(p, t_p)
+    expected = np.array([1., 3., 3.], dtype=np.float32)
+    assert np.allclose(t_p, expected)
 
-    print(p.transform_inv(np.array([1, 0, 0])))
+
+def test_pose_transform_inv():
+    p = pose.Pose(
+        np.array([1 / np.sqrt(2), 0, 0, 1 / np.sqrt(2)]),
+        np.array([1, 2, 3]),
+    )
+
+    t_p = p.transform_inv(np.array([1, 3, 3]))
+    expected = np.array([1., 0., 0.], dtype=np.float32)
+    assert np.allclose(t_p, expected)
 
 
 if __name__ == "__main__":
-    _test()
+    sys.exit(pytest.main(sys.argv))
--- qs_datasets/nuscenes/tmp.py
+++
@@ -1,9 +0,0 @@
-class A:
-    def __init__(self, foo: list) -> None:
-        self.foo = foo
-
-
-l = [1, 2, 3]
-a = A(l)
-l = []
-print(a.foo)
